457cb0d013d920dcf2dda82bb72f7924bda1105b
Samuel Mignot <sjmignot@gmail.com>
2024-07-31 23:50:37 -0700

    fix report print

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"fmt"
-  "os"
+	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
@@ -11,8 +11,8 @@ import (
 	"github.com/bojand/ghz/runner"
 	"github.com/chalk-ai/chalk-go"
 	commonv1 "github.com/chalk-ai/chalk-go/gen/chalk/common/v1"
+	enginev1 "github.com/chalk-ai/chalk-go/gen/chalk/engine/v1"
 	"github.com/chalk-ai/chalk-go/gen/chalk/engine/v1/enginev1connect"
-  enginev1 "github.com/chalk-ai/chalk-go/gen/chalk/engine/v1"
 
 	_ "github.com/goccy/go-json"
 	"github.com/spf13/cobra"
@@ -21,15 +21,14 @@ import (
 	structpb "google.golang.org/protobuf/types/known/structpb"
 )
 
-
 func curDir() string {
-    _, filename, _, ok := runtime.Caller(0)
+	_, filename, _, ok := runtime.Caller(0)
 
-    if !ok {
-      fmt.Println("Failed to find working directory")
-		  os.Exit(1)
-    }
-    return filepath.Dir(filename) + "/protos"
+	if !ok {
+		fmt.Println("Failed to find working directory")
+		os.Exit(1)
+	}
+	return filepath.Dir(filename)
 }
 
 // rootCmd represents the base command when called without any subcommands
@@ -38,123 +37,139 @@ var rootCmd = &cobra.Command{
 	Short: "Run load test for chalk grpc",
 	Long:  `This should be run on a node close to the client's sandbox`,
 	Run: func(cmd *cobra.Command, args []string) {
-    client := OrFatal(chalk.NewClient(&chalk.ClientConfig{
-      ApiServer: host,
-      ClientId: clientId,
-      ClientSecret: clientSecret,
-      EnvironmentId: environment,
-    }))("to create client")
+		client := OrFatal(chalk.NewClient(&chalk.ClientConfig{
+			ApiServer:     host,
+			ClientId:      clientId,
+			ClientSecret:  clientSecret,
+			EnvironmentId: environment,
+		}))("to create client")
 
-    tokenResult, err := client.GetToken()
-    grpcHost := strings.TrimPrefix(strings.TrimPrefix(tokenResult.Engines[tokenResult.PrimaryEnvironment], "https://"), "http://")
+		tokenResult, err := client.GetToken()
+		grpcHost := strings.TrimPrefix(strings.TrimPrefix(tokenResult.Engines[tokenResult.PrimaryEnvironment], "https://"), "http://")
 
+		cd := curDir()
 
-    cd := curDir()
+		var result *runner.Report
 
-    var result *runner.Report
+		if test {
+			pingRequest, err := proto.Marshal(&enginev1.PingRequest{Num: 10})
+			if err != nil {
+				fmt.Printf("Failed to marshal ping request with err: %s", err)
+				os.Exit(1)
+			}
+			result, err = runner.Run(
+				strings.TrimPrefix(enginev1connect.QueryServicePingProcedure, "/"),
+				fmt.Sprintf("%s:443", grpcHost),
+				runner.WithRPS(1),
+				runner.WithTotalRequests(1),
+				runner.WithMetadata(map[string]string{
+					"authorization":           fmt.Sprintf("Bearer %s", tokenResult.AccessToken),
+					"x-chalk-env-id":          tokenResult.PrimaryEnvironment,
+					"x-chalk-deployment-type": "engine-grpc",
+				}),
+				runner.WithProtoFile("./chalk/engine/v1/query_server.proto", []string{filepath.Join(cd, "protos")}),
+				runner.WithSkipTLSVerify(true),
+				runner.WithBinaryData(pingRequest),
+			)
+			if err != nil {
+				fmt.Printf("Failed to run Ping request with err: %s", err)
+				os.Exit(1)
+			}
 
-    if test {
-      pingRequest, err := proto.Marshal(&enginev1.PingRequest{Num: 10})
-        if err != nil {
-          fmt.Printf("Failed to marshal ping request with err: %s", err)
-          os.Exit(1)
-        }
-        result, err = runner.Run(
-          strings.TrimPrefix(enginev1connect.QueryServicePingProcedure, "/"),
-          fmt.Sprintf("%s:443", grpcHost),
-          runner.WithRPS(1),
-          runner.WithTotalRequests(1),
-          runner.WithMetadata(map[string]string{
-            "authorization":           fmt.Sprintf("Bearer %s", tokenResult.AccessToken),
-            "x-chalk-env-id":          tokenResult.PrimaryEnvironment,
-            "x-chalk-deployment-type": "engine-grpc",
-          }),
-          runner.WithProtoFile("./chalk/engine/v1/query_server.proto", []string{cd}),
-          runner.WithSkipTLSVerify(true),
-          runner.WithBinaryData(pingRequest),
-        )
-        if err != nil {
-          fmt.Printf("Failed to run Ping request with err: %s", err)
-          os.Exit(1)
-        }
+		} else {
+			if inputStr == nil && inputNum == nil {
+				fmt.Println("No inputs provided, please provide inputs with either the `--in_num` or the `--in_str` flags")
+				os.Exit(1)
+			}
+			inputsProcessed := make(map[string]*structpb.Value)
+			for k, v := range inputNum {
+				if inputsAreInts {
+					inputsProcessed[k] = structpb.NewNumberValue(float64(v))
+				}
+			}
 
-    } else {
-      if inputStr == nil && inputNum == nil {
-        fmt.Println("No inputs provided, please provide inputs with either the `--in_num` or the `--in_str` flags")
-        os.Exit(1)
-      }
-      inputsProcessed := make(map[string]*structpb.Value)
-      for k, v := range inputNum {
-        if inputsAreInts {
-          inputsProcessed[k] = structpb.NewNumberValue(float64(v))
-        }
-      }
+			for k, v := range inputStr {
+				if inputsAreInts {
+					inputsProcessed[k] = structpb.NewStringValue(v)
+				}
+			}
 
-      for k, v := range inputStr {
-        if inputsAreInts {
-          inputsProcessed[k] = structpb.NewStringValue(v)
-        }
-      }
+			outputsProcessed := make([]*commonv1.OutputExpr, len(output))
+			for i := 0; i < len(outputsProcessed); i++ {
+				outputsProcessed[i] = &commonv1.OutputExpr{
+					Expr: &commonv1.OutputExpr_FeatureFqn{
+						FeatureFqn: output[i],
+					},
+				}
+			}
 
-      outputsProcessed := make([]*commonv1.OutputExpr, len(output))
-      for i := 0; i < len(outputsProcessed); i++ {
-        outputsProcessed[i] = &commonv1.OutputExpr{
-          Expr: &commonv1.OutputExpr_FeatureFqn{
-            FeatureFqn: output[i],
-          },
-        }
-      }
+			oqr := commonv1.OnlineQueryRequest{
+				Inputs:  inputsProcessed,
+				Outputs: outputsProcessed,
+			}
+			if useNativeSql {
+				oqr.Context = &commonv1.OnlineQueryContext{Options: map[string]*structpb.Value{
+					"use_native_sql_operators": structpb.NewBoolValue(true),
+				}}
+			}
+			binaryData, err := proto.Marshal(&oqr)
+			if err != nil {
+				fmt.Printf("Failed to marshal online query request with inputs: '%s' and outputs: '%s'", inputsProcessed, outputsProcessed)
+				os.Exit(1)
+			}
 
-      oqr := commonv1.OnlineQueryRequest{
-        Inputs:  inputsProcessed,
-        Outputs: outputsProcessed,
-      }
-      if useNativeSql {
-        oqr.Context = &commonv1.OnlineQueryContext{Options: map[string]*structpb.Value{
-          "use_native_sql_operators": structpb.NewBoolValue(true),
-        }}
-      }
-      binaryData, err := proto.Marshal(&oqr)
-      if err != nil {
-        fmt.Printf("Failed to marshal online query request with inputs: '%s' and outputs: '%s'", inputsProcessed, outputsProcessed)
-        os.Exit(1)
-      }
+			total_requests := uint(float64(rps) * durationSeconds)
+			result, err = runner.Run(
+				strings.TrimPrefix(enginev1connect.QueryServiceOnlineQueryProcedure, "/"),
+				fmt.Sprintf("%s:443", grpcHost),
+				runner.WithRPS(rps),
+				runner.WithTotalRequests(total_requests),
+				runner.WithAsync(true),
+				runner.WithConnections(16),
+				runner.WithMetadata(map[string]string{
+					"authorization":           fmt.Sprintf("Bearer %s", tokenResult.AccessToken),
+					"x-chalk-env-id":          tokenResult.PrimaryEnvironment,
+					"x-chalk-deployment-type": "engine-grpc",
+				}),
+				runner.WithProtoFile("./chalk/engine/v1/query_server.proto", []string{filepath.Join(cd, "protos")}),
+				runner.WithSkipTLSVerify(true),
+				runner.WithConcurrency(16),
+				runner.WithBinaryData(binaryData),
+			)
+			if err != nil {
+				fmt.Printf("Failed to run online query with err: %s", err)
+				os.Exit(1)
+			}
+		}
 
-      total_requests := uint(float64(rps) * durationSeconds)
-      result, err = runner.Run(
-        strings.TrimPrefix(enginev1connect.QueryServiceOnlineQueryProcedure, "/"),
-        fmt.Sprintf("%s:443", grpcHost),
-        runner.WithRPS(rps),
-        runner.WithTotalRequests(total_requests),
-        runner.WithAsync(true),
-        runner.WithConnections(16),
-        runner.WithMetadata(map[string]string{
-          "authorization":           fmt.Sprintf("Bearer %s", tokenResult.AccessToken),
-          "x-chalk-env-id":          tokenResult.PrimaryEnvironment,
-          "x-chalk-deployment-type": "engine-grpc",
-        }),
-        runner.WithProtoFile("./chalk/engine/v1/query_server.proto", []string{cd}),
-        runner.WithSkipTLSVerify(true),
-        runner.WithConcurrency(16),
-        runner.WithBinaryData(binaryData),
-      )
-      ExitIfError(err, "failed to run query")
-    }
-    p := printer.ReportPrinter{
-      Out:    os.Stdout,
-      Report: result,
-    }
+		p := printer.ReportPrinter{
+			Out:    os.Stdout,
+			Report: result,
+		}
 
-    ExitIfError(p.Print("summary"), "failed to print report")
+		ExitIfError(p.Print("summary"), "failed to print report")
 
-    outputFile, err := os.OpenFile("report.html", os.O_RDWR, 0666)
-    ExitIfError(err, "to open report")
-    htmlSaver := printer.ReportPrinter{
-      Out:    outputFile,
-      Report: result,
-    }
-    ExitIfError(htmlSaver.Print("html"), "to save report")
-  },
+		outputFile, err := os.OpenFile(filepath.Join(filepath.Dir(cd), fmt.Sprintf("%s.html", strings.TrimSuffix(outputFile, ".html"))), os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
+		if err != nil {
+			fmt.Printf("Failed to open report file with error: %s", err)
+			os.Exit(1)
+		}
+
+		// prevents the bearer token from being printed out as part of the report
+		if removeReportMetadata {
+			result.Options.Metadata = nil
+		}
+
+		htmlSaver := printer.ReportPrinter{
+			Out:    outputFile,
+			Report: result,
+		}
+		err = htmlSaver.Print("html")
+		if err != nil {
+			fmt.Printf("Failed to save report with error: %s", err)
+			os.Exit(1)
+		}
+	},
 }
 
 func Execute() {
@@ -177,6 +192,7 @@ var output []string
 var environment string
 var outputFile string
 var useNativeSql bool
+var removeReportMetadata bool
 
 func init() {
 	viper.AutomaticEnv()
@@ -191,7 +207,8 @@ func init() {
 	flags.StringToStringVar(&inputStr, "in_str", nil, ", with port, for instance 'insert-your-host-here.chalk.ai:443'")
 	flags.StringToInt64Var(&inputNum, "in_num", nil, ", with port, for instance 'insert-your-host-here.chalk.ai:443'")
 	flags.StringArrayVar(&output, "out", nil, "to make requests to, with port, for instance 'insert-your-host-here.chalk.ai:443'")
-  flags.StringVarP(&outputFile, "output_file", "o", "result.html", "File name for saved report.")
-  flags.StringVar(&host, "host", "https://api.chalk.ai", "Default should not need to be updated.")
-  flags.BoolVar(&useNativeSql, "native_sql", false, "Whether to use the `use_native_sql_operators` planner option.")
+	flags.StringVarP(&outputFile, "output_file", "o", "result.html", "File name for saved report.")
+	flags.StringVar(&host, "host", "https://api.chalk.ai", "Default should not need to be updated.")
+	flags.BoolVar(&useNativeSql, "native_sql", false, "Whether to use the `use_native_sql_operators` planner option.")
+	flags.BoolVar(&removeReportMetadata, "remove_metadata", true, "Whether to use the `use_native_sql_operators` planner option.")
 }
